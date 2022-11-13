20ae2d2bf1ac33cdad9306639e36b17b7bff2e30
BanSoHee <bsh1004664@ewhain.net>
2022-11-13 15:50:15 +0900

    add del_stopwords.py

---
+++ helper function/del_stopwords.py
@@ -0,0 +1,8 @@
+def del_stop_words(text):
+
+    # 불용어
+    stopwords = ["도", "는", "다", "의", "가", "이", "은", "한", "에", "하", "고", "을", "를", "인", "듯", "과", "와", "네", "들", "듯", "지", "임", "게"]
+    # 불용어 제거
+    results = [text[i] for i in range(len(text)) if text[i] not in stopwords]
+
+    return results
