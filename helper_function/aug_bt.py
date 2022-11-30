448b869d496d39518d68c35164cecd9c8a84a04c
BanSoHee <bsh1004664@ewhain.net>
2022-11-30 22:06:54 +0900

    update aug_bt.py

--- helper_function/aug_bt.py
+++ helper_function/aug_bt.py
@@ -41,39 +41,42 @@ def get_translate(text, inlang, outlang):
     translator = Translator()
     trans = translator.translate(text, src=inlang, dest=outlang)
 
-    print(f'translation result : {trans.text}')
+    # print(f'translation result : {trans.text}')
 
     return trans.text
+
+# Back Translation
 
 # ko to en
 def BT_ko2en(text):
 
-    get_translate(text, 'ko', 'en')
+    out = get_translate(text, 'ko', 'en')
 
-    return
+    return out
 
 # en to ko
 def BT_en2ko(text):
 
-    get_translate(text, 'en', 'ko')
+    out = get_translate(text, 'en', 'ko')
 
-    return
+    return out
 
 # ko to jp
 def BT_ko2jp(text):
 
-    get_translate(text, 'ko', 'jp')
+    out = get_translate(text, 'ko', 'jp')
 
-    return
+    return out
 
 # jp to ko
 def BT_jp2ko(text):
 
-    get_translate(text, 'jp', 'ko')
+    out = get_translate(text, 'jp', 'ko')
 
-    return
+    return out
 
 
 ''' sample '''
 # get_translate('안녕하세요!', 'ko', 'en')
-# BT_en2ko('hello!')
+# out = BT_en2ko('hello!')
+# print(out)
